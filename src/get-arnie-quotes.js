e35b01dae4e69fbc88cd66b70ad0fd9b48d51841
Felix <felikslyu@gmail.com>
2025-07-31 20:57:16 +1000

    Implement getArnieQuotes to fetch and transform HTTP responses concurrently

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,19 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const promises = urls.map(async (url) => {
+    const res = await httpGet(url);
+    const message = JSON.parse(res.body).message;
+
+    if (res.status === 200) {
+      return { 'Arnie Quote': message };
+    } else {
+      return { 'FAILURE': message };
+    }
+  });
+
+  const results = Promise.all(promises);
+  return results;
 };
 
 module.exports = {
